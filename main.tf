bd775433062b694d1a345136ad25f884858c3c35
nomansadiq11 <nomi.cs786@gmail.com>
2019-11-15 22:52:37 +0400

    initial commit - all serverless services on azure

---
+++ main.tf
@@ -0,0 +1,115 @@
+resource "azurerm_resource_group" "serverless-rg" {
+  name     = "${var.resouce_group_name}"
+  location = "${var.location}"
+
+  tags = {
+    environment = "${var.tag}"
+  }
+}
+
+
+resource "azurerm_storage_account" "SA_serverless" {
+  name                     = "saserverless"
+  resource_group_name      = "${azurerm_resource_group.serverless-rg.name}"
+  location                 = "${var.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+   tags = {
+    environment = "${var.tag}"
+  }
+
+}
+
+
+## Payment response update
+
+
+resource "azurerm_app_service_plan" "ASP_Serverless_fn" {
+  name                = "Serverless_fn"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+
+   tags = {
+    environment = "${var.tag}"
+  }
+}
+
+
+## Cosmos DB create account
+
+
+
+resource "azurerm_cosmosdb_account" "cosmos_serverless" {
+  name                = "serverless-${random_integer.ri.result}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+  offer_type          = "Standard"
+  kind                = "GlobalDocumentDB"
+
+  enable_automatic_failover = false
+  enable_multiple_write_locations = false
+
+
+  consistency_policy {
+    consistency_level       = "Session"
+    max_interval_in_seconds = 5
+    max_staleness_prefix    = 100
+  }
+
+  geo_location {
+    location          = "${var.failover_location}"
+    failover_priority = 0
+  }
+
+  # geo_location {
+  #   prefix            = "paymentfacadedev-db-${random_integer.ri.result}-customid"
+  #   location          = "${var.location}"
+  #   failover_priority = 0
+  # }
+
+
+}
+
+
+
+## Cosmos DB  create account
+
+
+
+## Service BUS
+
+resource "azurerm_servicebus_namespace" "SBserverless" {
+  name                = "SBServerless"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+  sku                 = "Standard"
+
+  tags = {
+    source = "${var.tag}"
+  }
+}
+
+resource "azurerm_servicebus_queue" "messagequeue" {
+  name                = "messagequeue"
+  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+  namespace_name      = "${azurerm_servicebus_namespace.SBserverless.name}"
+
+  enable_partitioning = true
+}
+
+
+
+resource "azurerm_servicebus_topic" "topicname" {
+  name                = "topicname"
+  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+  namespace_name      = "${azurerm_servicebus_namespace.SBserverless.name}"
+
+  enable_partitioning = true
+}
+
---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "resouce_group_name" {
+  default="Serverless"
+}
+
+variable "location" {
+  default = "West Europe"
+}
+
+
+variable "tag" {
+  default = "Dev"
+}
+
+
+variable "failover_location" {
+  default = "North Europe"
+}
+
+
+resource "random_integer" "ri" {
+  min = 10000
+  max = 99999
+}
