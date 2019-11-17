d15c5ce595c607dbd27e026bef08a40d675ffbf3
nomansadiq11 <nomi.cs786@gmail.com>
2019-11-18 00:37:22 +0100

    remote state added

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "serverless-rg" {
 
 
 resource "azurerm_storage_account" "SA_serverless" {
-  name                     = "saserverless"
+  name                     = "sa${random_integer.ri.result}"
   resource_group_name      = "${azurerm_resource_group.serverless-rg.name}"
   location                 = "${var.location}"
   account_tier             = "Standard"
@@ -22,17 +22,16 @@ resource "azurerm_storage_account" "SA_serverless" {
 }
 
 
-## Payment response update
 
-
-resource "azurerm_app_service_plan" "ASP_Serverless_fn" {
-  name                = "Serverless_fn"
+resource "azurerm_app_service_plan" "ASP_Serverless_For_AF" {
+  name                = "ASPFAF"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
 
   sku {
-    tier = "Standard"
-    size = "S1"
+    tier = "Basic"
+    size = "B1"
+    capacity = 1
   }
 
    tags = {
@@ -41,31 +40,43 @@ resource "azurerm_app_service_plan" "ASP_Serverless_fn" {
 }
 
 
-## Cosmos DB create account
+resource "azurerm_function_app" "AF_serverless" {
+  name                      = "af${random_string.fqdn.result}"
+  location                  = "${var.location}"
+  resource_group_name       = "${azurerm_resource_group.serverless-rg.name}"
+  app_service_plan_id       = "${azurerm_app_service_plan.ASP_Serverless_For_AF.id}"
+  storage_connection_string = "${azurerm_storage_account.SA_serverless.primary_connection_string}"
 
-
-
-resource "azurerm_cosmosdb_account" "cosmos_serverless" {
-  name                = "serverless-${random_integer.ri.result}"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
-  offer_type          = "Standard"
-  kind                = "GlobalDocumentDB"
-
-  enable_automatic_failover = false
-  enable_multiple_write_locations = false
-
-
-  consistency_policy {
-    consistency_level       = "Session"
-    max_interval_in_seconds = 5
-    max_staleness_prefix    = 100
+   tags = {
+    environment = "${var.tag}"
   }
+}
 
-  geo_location {
-    location          = "${var.failover_location}"
-    failover_priority = 0
-  }
+
+# resource "azurerm_cosmosdb_account" "cosmos_serverless" {
+#   name                = "serverless-${random_integer.ri.result}"
+#   location            = "${var.location}"
+#   resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+#   offer_type          = "Standard"
+#   kind                = "GlobalDocumentDB"
+
+#   enable_automatic_failover = false
+#   enable_multiple_write_locations = false
+
+
+#   consistency_policy {
+#     consistency_level       = "Session"
+#     max_interval_in_seconds = 5
+#     max_staleness_prefix    = 100
+#   }
+
+#   geo_location {
+#     location          = "${var.failover_location}"
+#     failover_priority = 0
+#   }
+
+
+## Commented geo location for development purpose
 
   # geo_location {
   #   prefix            = "paymentfacadedev-db-${random_integer.ri.result}-customid"
@@ -74,7 +85,7 @@ resource "azurerm_cosmosdb_account" "cosmos_serverless" {
   # }
 
 
-}
+# }
 
 
 
@@ -84,77 +95,68 @@ resource "azurerm_cosmosdb_account" "cosmos_serverless" {
 
 ## Service BUS
 
-resource "azurerm_servicebus_namespace" "SBserverless" {
-  name                = "SBServerless"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
-  sku                 = "Standard"
+# resource "azurerm_servicebus_namespace" "SBserverless" {
+#   name                = "SBServerless"
+#   location            = "${var.location}"
+#   resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+#   sku                 = "Standard"
 
-  tags = {
-    source = "${var.tag}"
-  }
-}
+#   tags = {
+#     source = "${var.tag}"
+#   }
+# }
 
-resource "azurerm_servicebus_queue" "messagequeue" {
-  name                = "messagequeue"
-  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
-  namespace_name      = "${azurerm_servicebus_namespace.SBserverless.name}"
+# resource "azurerm_servicebus_queue" "messagequeue" {
+#   name                = "messagequeue"
+#   resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+#   namespace_name      = "${azurerm_servicebus_namespace.SBserverless.name}"
 
-  enable_partitioning = true
-}
-
-
-resource "azurerm_resource_group" "testwebapp" {
-  name     = "teswebapp"
-  location = "${var.location}"
-
-  tags = {
-    environment = "${var.tag}"
-  }
-}
+#   enable_partitioning = true
+# }
 
 
-resource "azurerm_servicebus_topic" "topicname" {
-  name                = "topicname"
-  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
-  namespace_name      = "${azurerm_servicebus_namespace.SBserverless.name}"
 
-  enable_partitioning = true
-}
+# resource "azurerm_servicebus_topic" "topicname" {
+#   name                = "topicname"
+#   resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+#   namespace_name      = "${azurerm_servicebus_namespace.SBserverless.name}"
+
+#   enable_partitioning = true
+# }
 
 
 
 # Web App services
 
 
-# resource "azurerm_app_service_plan" "serverless_ASP" {
-#   name                = "${random_string.fqdn.result}"
-#   location            = "${var.location}"
-#   resource_group_name = "${azurerm_resource_group.testwebapp.name}"
-#   kind                = "Linux"
-#   reserved            = true
+resource "azurerm_app_service_plan" "ASP_For_WebApp" {
+  name                = "ASPWEB${random_string.fqdn.result}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+  kind                = "Linux"
+  reserved            = true
   
-#   sku {
-#     tier = "Basic"
-#     size = "B1"
-#     capacity = 1
-#   }
-# }
+  sku {
+    tier = "Basic"
+    size = "B1"
+    capacity = 1
+  }
+}
 
-# resource "azurerm_app_service" "serverlesswebapp_AS" {
-#   name                = "${random_string.fqdn.result}"
-#   location            = "${var.location}"
-#   resource_group_name = "${azurerm_resource_group.testwebapp.name}"
-#   app_service_plan_id = "${azurerm_app_service_plan.ASP_Serverless_fn.id}"
+resource "azurerm_app_service" "AS_Webapp" {
+  name                = "WA${random_string.fqdn.result}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.serverless-rg.name}"
+  app_service_plan_id = "${azurerm_app_service_plan.ASP_For_WebApp.id}"
 
-#   site_config {
-#     dotnet_framework_version    = "v4.0"
-#     scm_type                    = "None"
-#     linux_fx_version            = "DOTNETCORE|3.0"
-#     ftps_state                  = "AllAllowed"
-#   }
+  site_config {
+    dotnet_framework_version    = "v4.0"
+    scm_type                    = "None"
+    linux_fx_version            = "DOTNETCORE|3.0"
+    ftps_state                  = "AllAllowed"
+  }
 
   
-# }
+}
 
 # web app service
