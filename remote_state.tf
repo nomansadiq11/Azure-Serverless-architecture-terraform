784bbdefbe28b399229678f27d94f999da43cd72
nomansadiq11 <nomi.cs786@gmail.com>
2020-05-15 17:35:17 +0400

    remove remove state and add provider information

--- remote_state.tf
+++ remote_state.tf
@@ -1,8 +1,8 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "ForTerraformStates"
-    storage_account_name = "manageterraformstates"
-    container_name       = "terraformprojectsstates"
-    key                  = "serverless.terraform.tfstate"
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     resource_group_name  = "ForTerraformStates"
+#     storage_account_name = "manageterraformstates"
+#     container_name       = "terraformprojectsstates"
+#     key                  = "serverless.terraform.tfstate"
+#   }
+# }
